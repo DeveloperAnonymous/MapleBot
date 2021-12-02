374c8737eb13fb54c7a192f270e1c94330db0651
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-12-02 00:34:07 -0500

    update command to updates the bot for me because I'm lazy

--- bot/commands/moderation.py
+++ bot/commands/moderation.py
@@ -1,3 +1,8 @@
+import platform
+import asyncio
+import discord
+import os
+
 from discord.ext import commands
 
 class Moderation(commands.Cog):
@@ -13,3 +18,47 @@ class Moderation(commands.Cog):
         spoiler_author = spoiler_message.author
         await spoiler_message.delete()
         await ctx.send(f"Please, {spoiler_author.mention}, don't spoil the others!")
+
+    @commands.command()
+    @commands.is_owner()
+    async def update(self, ctx: commands.Context):
+        """
+        Updates the bot for me because I'm lazy.
+        
+        This command is only available for the bot owner.
+        """
+
+        try:
+            sys = platform.platform()
+            if "Linux" in sys:
+                update_result = await asyncio.create_subprocess_shell('bash update_script.sh',
+                                                                    stdout=asyncio.subprocess.PIPE,
+                                                                    stderr=asyncio.subprocess.PIPE)
+            elif "Windows" in sys:
+                update_result = await asyncio.create_subprocess_shell(
+                    '"D:\\Softwares\\Git\\bin\\bash" update_script.sh',
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE)
+            else:
+                raise asyncio.CancelledError()
+        except asyncio.CancelledError as updateexc:
+            update_result = updateexc.output
+        stdout, stderr = await update_result.communicate()
+        if stdout:
+            update_result = stdout.decode("utf-8")
+        elif stderr:
+            update_result = stderr.decode("utf-8")
+        else:
+            update_result = "Something went wrong!"
+
+        if len(update_result.strip()) == 0:
+            update_result = "No output."
+
+        update_embed = discord.Embed(title=":gear: Updating!")
+        update_embed.description = "Pulling lastest version from **github**!"
+        update_embed.add_field(name='Changes', value='```' + update_result + '```', inline=False)
+        await ctx.reply(embed=update_embed)
+
+        update_result = update_result.strip()
+        if not ("is up to date." in update_result or update_result == "Something went wrong!"):
+            os._exit(1)
