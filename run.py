efaaec87ee902006993f0bed607f86550aea1cc5
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-12-02 01:07:50 -0500

    Removed dead code

--- run.py
+++ run.py
@@ -23,8 +23,6 @@ class Bot(commands.Bot):
             discord.Activity(type=discord.ActivityType.watching, name="the market")
         ])
 
-        # asyncio.new_event_loop().run_until_complete(self.on_new_alert())
-
     async def on_ready(self):
         print(f"Logged as {self.user.name} #{self.user.id}")
 
@@ -41,15 +39,6 @@ class Bot(commands.Bot):
         util.logger.error("Something went wrong while changing status, restarting loop in 5 seconds...")
         asyncio.sleep(5)
         self.change_status.start()
-
-    @tasks.loop(minutes=1)
-    async def update_tracked_items(self):
-        pass
-
-    # async def on_new_alert(self):
-    #     async with websockets.connect(f"ws://{configs.WS_HOST}:{configs.WS_PORT}/") as ws:
-    #         msg = await ws.recv()
-    #         util.logger.info(msg)
 
 
 if __name__ == "__main__":
