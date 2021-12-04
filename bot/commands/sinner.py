ef3341739c57b37c8c4674d2cdf0abfaedb61687
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-12-04 15:16:36 -0500

    Bot will now reply Urianger style

--- bot/commands/sinner.py
+++ bot/commands/sinner.py
@@ -1,14 +1,15 @@
 from difflib import SequenceMatcher
 
+import discord
 from discord.ext import commands
 
 PRAYER = "Excuse me father, for I have sinned."
 
 class Sinner(commands.Cog):
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
 
         if SequenceMatcher(None, message.content, PRAYER).ratio() > 0.8:
-            await message.channel.send("Yes, my child, I forgive you :pray:")
+            await message.channel.send("Aye, mine own issue, i f'rgive thee :pray:")
